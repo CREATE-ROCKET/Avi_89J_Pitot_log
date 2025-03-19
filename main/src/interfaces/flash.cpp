367cd55988eb4f24dad7d8917f56047ec8aaa648
Natsu-B <natsu.minatomirai@gmail.com>
2025-03-19 17:08:01 +0900

    [remove] SPIFFS support

--- main/src/interfaces/flash.cpp
+++ main/src/interfaces/flash.cpp
@@ -3,8 +3,6 @@
 #include "debug.h"
 #include "S25FL127S 1.0.0/src/SPIflash.h"
 #include <Arduino.h>
-#include <SPIFFS.h>
-#include <FS.h>
 #include <esp_heap_caps.h>
 #include <esp_task_wdt.h>
 #include "task_queue.h"
@@ -22,7 +20,6 @@ constexpr int data_size = numof_maxData * (sizeof(Data) / sizeof(uint8_t));
 int position = 0; // SPIflashの書き込み位置
 constexpr int FLASH_BLOCK_SIZE = 0x100;
 constexpr uint32_t SPI_FLASH_MAX_ADDRESS = 0x2000000;
-constexpr char num_path[] = "/number.txt";
 
 String SPIFFSpath;
 
@@ -36,94 +33,94 @@ union PitotDataUnion
 namespace flash
 {
     // 基本的にSDと同様
-    int makeNewFile()
-    {
-        int number = 0;
-        File file = SPIFFS.open(num_path);
-        int result = file.read();
-        if (result != -1)
-        { // ファイルが存在する場合読み取る
-            String number_txt = "";
-            while (file.available())
-            {
-                number_txt.concat((char)file.read());
-            }
-            if (!number_txt.length())
-            {
-                pr_debug("number.txt maybe broken: %s", number_txt.c_str());
-                return 1;
-            }
-            number = number_txt.toInt();
-            if (!number)
-            {
-                pr_debug("number.txt maybe contain nonInt: %s", number_txt.c_str());
-                return 2;
-            }
-        }
-        else // /spiffs/number.txt ファイルが存在しない場合
-            pr_debug("Cannot find number.txt assume the SPIFFS is init. creating...");
-        file.close();
+    // int makeNewFile()
+    // {
+    //     int number = 0;
+    //     File file = SPIFFS.open(num_path);
+    //     int result = file.read();
+    //     if (result != -1)
+    //     { // ファイルが存在する場合読み取る
+    //         String number_txt = "";
+    //         while (file.available())
+    //         {
+    //             number_txt.concat((char)file.read());
+    //         }
+    //         if (!number_txt.length())
+    //         {
+    //             pr_debug("number.txt maybe broken: %s", number_txt.c_str());
+    //             return 1;
+    //         }
+    //         number = number_txt.toInt();
+    //         if (!number)
+    //         {
+    //             pr_debug("number.txt maybe contain nonInt: %s", number_txt.c_str());
+    //             return 2;
+    //         }
+    //     }
+    //     else // /spiffs/number.txt ファイルが存在しない場合
+    //         pr_debug("Cannot find number.txt assume the SPIFFS is init. creating...");
+    //     file.close();
 
-        File file1 = SPIFFS.open(num_path, FILE_WRITE);
-        if (!file1)
-        {
-            pr_debug("Failed to open file!!!");
-            return 3;
-        }
-        if (!file1.print(number + 1))
-        {
-            pr_debug("Failed to write file!!!");
-            return 4;
-        }
-        file1.close();
-        SPIFFSpath = "/" + String(number) + ".csv";
-        pr_debug("SPIFFS path: %s", SPIFFSpath.c_str());
-        File tmp = SPIFFS.open(SPIFFSpath, FILE_WRITE);
-        if (!tmp)
-        {
-            pr_debug("failed to open file");
-            return 5;
-        }
-        if (!tmp.print("time, pascal, temperature\n"))
-        {
-            pr_debug("failed to write file");
-            return 6;
-        }
-        tmp.close();
-        return 0;
-    }
+    //     File file1 = SPIFFS.open(num_path, FILE_WRITE);
+    //     if (!file1)
+    //     {
+    //         pr_debug("Failed to open file!!!");
+    //         return 3;
+    //     }
+    //     if (!file1.print(number + 1))
+    //     {
+    //         pr_debug("Failed to write file!!!");
+    //         return 4;
+    //     }
+    //     file1.close();
+    //     SPIFFSpath = "/" + String(number) + ".csv";
+    //     pr_debug("SPIFFS path: %s", SPIFFSpath.c_str());
+    //     File tmp = SPIFFS.open(SPIFFSpath, FILE_WRITE);
+    //     if (!tmp)
+    //     {
+    //         pr_debug("failed to open file");
+    //         return 5;
+    //     }
+    //     if (!tmp.print("time, pascal, temperature\n"))
+    //     {
+    //         pr_debug("failed to write file");
+    //         return 6;
+    //     }
+    //     tmp.close();
+    //     return 0;
+    // }
 
-    int SPIFFS_init()
-    {
-        // 初期化されているため、ファイル名は0から始まる
-        File numberHandle = SPIFFS.open(num_path, "w");
-        if (!numberHandle)
-        {
-            pr_debug("%s cannot open", num_path);
-            return 1;
-        }
-        if (!numberHandle.print("1"))
-        {
-            pr_debug("failed to write file");
-            return 2;
-        }
-        numberHandle.close();
-        SPIFFSpath = "/0.csv";
-        pr_debug("SPIFFS path: %s", SPIFFSpath.c_str());
-        File tmp = SPIFFS.open(SPIFFSpath, FILE_WRITE);
-        if (!tmp)
-        {
-            pr_debug("failed to open file");
-            return 5;
-        }
-        if (!tmp.print("time, pascal, temperature\n"))
-        {
-            pr_debug("failed to write file");
-            return 6;
-        }
-        tmp.close();
-        return 0;
-    }
+    // int SPIFFS_init()
+    // {
+    //     // 初期化されているため、ファイル名は0から始まる
+    //     File numberHandle = SPIFFS.open(num_path, "w");
+    //     if (!numberHandle)
+    //     {
+    //         pr_debug("%s cannot open", num_path);
+    //         return 1;
+    //     }
+    //     if (!numberHandle.print("1"))
+    //     {
+    //         pr_debug("failed to write file");
+    //         return 2;
+    //     }
+    //     numberHandle.close();
+    //     SPIFFSpath = "/0.csv";
+    //     pr_debug("SPIFFS path: %s", SPIFFSpath.c_str());
+    //     File tmp = SPIFFS.open(SPIFFSpath, FILE_WRITE);
+    //     if (!tmp)
+    //     {
+    //         pr_debug("failed to open file");
+    //         return 5;
+    //     }
+    //     if (!tmp.print("time, pascal, temperature\n"))
+    //     {
+    //         pr_debug("failed to write file");
+    //         return 6;
+    //     }
+    //     tmp.close();
+    //     return 0;
+    // }
 
     PitotDataUnion pitotData;
     uint8_t tmp[256];
@@ -169,87 +166,87 @@ namespace flash
         return 0;
     }
 
-    int get_SPIFFS_old_data()
-    {
-        File root = SPIFFS.open("/");
-        if (!root)
-        {
-            pr_debug("failed to open root dir");
-            return 1;
-        }
-        pr_debug("listing SPIFFS dir...");
-        File file = root.openNextFile();
-        while (file)
-        {
-            if (file.isDirectory())
-            {
-                pr_debug("no directory expected. but found");
-                break;
-            }
-            if (!file.available())
-            {
-                pr_debug("not available file found");
-                continue;
-            }
-            const char *filename = file.name();
-            if (strcmp(filename, "number.txt") || strcmp(filename, num_path))
-                continue;
-            char *end;
-            int filecount = strtol(filename, &end, 10);
-            if (*end != '.')
-            {
-                pr_debug("failed to convert filename");
-                return 2;
-            }
-            size_t filesize = file.size();
-            char *buffer;
-            if (psramFound())
-            {
-                buffer = (char *)heap_caps_malloc(filesize + 1, MALLOC_CAP_SPIRAM);
-            }
-            else
-            {
-                pr_debug("no PSRAM found");
-                buffer = (char *)malloc(filesize + 1);
-            }
-            if (!buffer)
-            {
-                pr_debug("failed to malloc");
-                return 3;
-            }
+    // int get_SPIFFS_old_data()
+    // {
+    //     File root = SPIFFS.open("/");
+    //     if (!root)
+    //     {
+    //         pr_debug("failed to open root dir");
+    //         return 1;
+    //     }
+    //     pr_debug("listing SPIFFS dir...");
+    //     File file = root.openNextFile();
+    //     while (file)
+    //     {
+    //         if (file.isDirectory())
+    //         {
+    //             pr_debug("no directory expected. but found");
+    //             break;
+    //         }
+    //         if (!file.available())
+    //         {
+    //             pr_debug("not available file found");
+    //             continue;
+    //         }
+    //         const char *filename = file.name();
+    //         if (strcmp(filename, "number.txt") || strcmp(filename, num_path))
+    //             continue;
+    //         char *end;
+    //         int filecount = strtol(filename, &end, 10);
+    //         if (*end != '.')
+    //         {
+    //             pr_debug("failed to convert filename");
+    //             return 2;
+    //         }
+    //         size_t filesize = file.size();
+    //         char *buffer;
+    //         if (psramFound())
+    //         {
+    //             buffer = (char *)heap_caps_malloc(filesize + 1, MALLOC_CAP_SPIRAM);
+    //         }
+    //         else
+    //         {
+    //             pr_debug("no PSRAM found");
+    //             buffer = (char *)malloc(filesize + 1);
+    //         }
+    //         if (!buffer)
+    //         {
+    //             pr_debug("failed to malloc");
+    //             return 3;
+    //         }
 
-            file.read((uint8_t *)buffer, filesize);
-            buffer[filesize] = '\0';
-            SD_Data *data_wrapper = new SD_Data;
-            data_wrapper->data = buffer;
-            data_wrapper->type = data_type::data_type_SPIFFS + filecount;
-            if (xQueueSend(ParityToSDQueue, &data_wrapper, portMAX_DELAY) != pdTRUE)
-            {
-                pr_debug("failed to send");
-                return 4;
-            }
-            file = file.openNextFile();
-        }
-        return 0;
-    }
+    //         file.read((uint8_t *)buffer, filesize);
+    //         buffer[filesize] = '\0';
+    //         SD_Data *data_wrapper = new SD_Data;
+    //         data_wrapper->data = buffer;
+    //         data_wrapper->type = data_type::data_type_SPIFFS + filecount;
+    //         if (xQueueSend(ParityToSDQueue, &data_wrapper, portMAX_DELAY) != pdTRUE)
+    //         {
+    //             pr_debug("failed to send");
+    //             return 4;
+    //         }
+    //         file = file.openNextFile();
+    //     }
+    //     return 0;
+    // }
 
-    int appendfile(char *message)
-    {
-        pr_debug("Appending to file: %s", SPIFFSpath.c_str());
-        File file = SPIFFS.open(SPIFFSpath, FILE_APPEND);
-        if (!file)
-        {
-            pr_debug("Failed to open file for appending");
-            return 1;
-        }
-        if (!file.print(message))
-        {
-            pr_debug("data append failed");
-            return 2;
-        }
-        file.close();
-        return 0;
-    }
+    // int appendfile(char *message)
+    // {
+    //     pr_debug("Appending to file: %s", SPIFFSpath.c_str());
+    //     File file = SPIFFS.open(SPIFFSpath, FILE_APPEND);
+    //     if (!file)
+    //     {
+    //         pr_debug("Failed to open file for appending");
+    //         return 1;
+    //     }
+    //     if (!file.print(message))
+    //     {
+    //         pr_debug("data append failed");
+    //         return 2;
+    //     }
+    //     file.close();
+    //     return 0;
+    // }
 
     int init()
     {
@@ -259,22 +256,22 @@ namespace flash
             return 1;
         }
         flash1.begin(&SPIC1, CS, SPIFREQ);
-        if (!SPIFFS.begin(true))
-        {
-            pr_debug("Failed to init SPIFFS");
-            return 2;
-        }
+        // if (!SPIFFS.begin(true))
+        // {
+        //     pr_debug("Failed to init SPIFFS");
+        //     return 2;
+        // }
 #ifdef DEBUG
-        size_t total, used;
-        total = SPIFFS.totalBytes();
-        used = SPIFFS.usedBytes();
-        pr_debug("SPIFFS\n\ttotal size:%u\nused size:\t%u", total, used);
+//         size_t total, used;
+//         total = SPIFFS.totalBytes();
+//         used = SPIFFS.usedBytes();
+//         pr_debug("SPIFFS\n\ttotal size:%u\nused size:\t%u", total, used);
 #endif
-        int makefileresult = makeNewFile();
-        if (makefileresult)
-        {
-            pr_debug("failed to make new file in SPIFFS: %d", makefileresult);
-        }
+        // int makefileresult = makeNewFile();
+        // if (makefileresult)
+        // {
+        //     pr_debug("failed to make new file in SPIFFS: %d", makefileresult);
+        // }
         return 0;
     }
 
@@ -297,12 +294,12 @@ namespace flash
                     continue;
                 }
                 flash1.write(position, pitotData);
-                char *tmp = cmn_task::DataToChar((Data *)pitotData);
-                if (tmp)
-                {
-                    appendfile(tmp);
-                    delete[] tmp;
-                }
+                // char *tmp = cmn_task::DataToChar((Data *)pitotData);
+                // if (tmp)
+                // {
+                //     appendfile(tmp);
+                //     delete[] tmp;
+                // }
                 delete[] pitotData;
                 position += FLASH_BLOCK_SIZE;
             }
@@ -331,20 +328,20 @@ namespace flash
         esp_task_wdt_delete(xTaskGetCurrentTaskHandle());
         flash1.erase();
         position = 0;
-        can::canSend('1');
-        if (!SPIFFS.format())
-        {
-            can::canSend('F');
-            error_log("failed to format SPIFFS");
-        }
-        esp_task_wdt_add(xTaskGetCurrentTaskHandle());
-        can::canSend('2');
-        int hoge = SPIFFS_init();
-        if (hoge)
-        {
-            can::canSend('F');
-            error_log("failed to make new file: %d", hoge);
-        }
+        // can::canSend('1');
+        // if (!SPIFFS.format())
+        // {
+        //     can::canSend('F');
+        //     error_log("failed to format SPIFFS");
+        // }
+        // esp_task_wdt_add(xTaskGetCurrentTaskHandle());
+        // can::canSend('2');
+        // int hoge = SPIFFS_init();
+        // if (hoge)
+        // {
+        //     can::canSend('F');
+        //     error_log("failed to make new file: %d", hoge);
+        // }
 #endif
     }
 }
